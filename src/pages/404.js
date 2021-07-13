4c63de358c413734c88b27255af1df05cbbcd0da
adebayus <adebayus17@gmail.com>
2021-07-13 11:51:59 +0700

    Navbar & Links

---
+++ src/pages/404.js
@@ -0,0 +1,6 @@
+import React from "react"
+function NotFound() {
+  return <div></div>
+}
+
+export default NotFound
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
-
+import Navbar from "./components/navbar"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <Navbar />
+      <div>
+        <h2>Design</h2>
+        <h3>Develop</h3>
+      </div>
+    </section>
+  )
 }
